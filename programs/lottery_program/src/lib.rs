ec758450edc95bdff7630788f85a5dda833f93a9
Jaymanyoo Singh Chouhan <jaymanyoo@Jaymanyoos-Mac-mini.local>
2022-07-09 19:29:37 +0530

    Create Lottery with test case

--- programs/lottery_program/src/lib.rs
+++ programs/lottery_program/src/lib.rs
@@ -12,7 +12,23 @@ pub mod lottery_program {
 
         program_state.lottery_count = 0;
         program_state.owner = *owner.key;
-        
+
+        Ok(())
+    }
+
+    pub fn create_lottery(ctx: Context<CreateLottery>) -> Result<()> {
+        let program_state = &mut ctx.accounts.program_state;
+        let user = &mut ctx.accounts.user;
+        let lottery = &mut ctx.accounts.lottery;
+
+        // check if the program_state "owner" is equal to "user"
+        require!(*user.key == program_state.owner, LotteryError::NotAManager);
+
+        lottery.participant_count = 0;
+        lottery.is_active = true;
+
+        program_state.lottery_count += 1;
+
         Ok(())
     }
 }
@@ -34,9 +50,47 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>
 }
 
+#[derive(Accounts)]
+pub struct CreateLottery<'info> {
+    #[account(
+        mut,
+        seeds=[b"state".as_ref()],
+        bump
+    )]
+    pub program_state: Account<'info, ProgramState>,
+
+    #[account(
+        init,
+        payer=user,
+        space=8+32+32+64+2,
+        seeds=[b"lottery".as_ref(), program_state.lottery_count.to_be_bytes().as_ref()],
+        bump
+    )]
+    pub lottery: Account<'info, Lottery>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
 
 #[account]
 pub struct ProgramState {
     pub owner: Pubkey,
     pub lottery_count: u64
+}
+
+#[account]
+pub struct Lottery {
+    pub escrow: Pubkey,
+    pub winner: Pubkey,
+    pub participant_count: u64,
+    pub is_active: bool,
+}
+
+#[error_code]
+pub enum LotteryError {
+    #[msg("Restricted! You are not having Manager privilege.")]
+    NotAManager
 }
--- tests/lottery_program.ts
+++ tests/lottery_program.ts
@@ -25,10 +25,82 @@ describe("lottery_program", () => {
       owner: owner_key,
       systemProgram: system_key
     })
-    .rpc();
+      .rpc();
     console.log("Your initialized transaction signature", tx);
 
     let created_state = await program.account.programState.fetch(account_state_signer);
     assert(created_state.owner.toString() === owner_key.toString(), "Owner do not match.")
   });
+
+  it("lottery room created!", async () => {
+
+    let [account_state_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("state")],
+      program.programId
+    )
+    let account_state_data = await program.account.programState.fetch(account_state_pda);
+
+    let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("lottery"), account_state_data.lotteryCount.toBuffer('be', 8)],
+      program.programId
+    )
+    const tx = await program.methods.createLottery()
+      .accounts({
+        programState: account_state_pda,
+        lottery: lottery_pda,
+        user: owner_key,
+        systemProgram: system_key
+      })
+      .rpc();
+
+      let created_lottery = await program.account.lottery.fetch(lottery_pda);
+      account_state_data = await program.account.programState.fetch(account_state_pda);
+      assert(created_lottery.participantCount.toString() === "0", "Wrong Lottery Participant count.")
+      assert(account_state_data.lotteryCount.toString() === "1", "Wrong Lottery count in program.")
+  });
+
+  it("Non-manager fails creating lottery room!", async () => {
+
+    let [account_state_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("state")],
+      program.programId
+    )
+    let account_state_data = await program.account.programState.fetch(account_state_pda);
+
+    let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("lottery"), account_state_data.lotteryCount.toBuffer('be', 8)],
+      program.programId
+    )
+
+    // create a unauthorised user
+    let bad_actor = anchor.web3.Keypair.generate();
+    await fundAccount(bad_actor);
+
+    try{
+      const tx = await program.methods.createLottery()
+      .accounts({
+        programState: account_state_pda,
+        lottery: lottery_pda,
+        user: bad_actor.publicKey,
+        systemProgram: system_key
+      })
+      .signers([bad_actor])
+      .rpc();
+
+      assert(false, "unauthorised user created lottery!")
+    }catch(err){
+      // console.log(err);
+      
+    }
+    
+  });
 });
+
+
+const fundAccount = async (userKey) => {
+  const connection = provider.connection;
+  let signature = await connection.requestAirdrop(userKey.publicKey, 1000953520)
+  await connection.confirmTransaction(signature);
+
+  let balance = await connection.getBalance(userKey.publicKey);
+}
