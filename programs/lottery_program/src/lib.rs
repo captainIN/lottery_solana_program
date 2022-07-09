d3aeb7411748db9d14b54b23a3e08d374d0e8882
Jaymanyoo Singh Chouhan <jaymanyoo@Jaymanyoos-Mac-mini.local>
2022-07-09 16:55:13 +0530

    ititialise state logic and its test case

--- programs/lottery_program/src/lib.rs
+++ programs/lottery_program/src/lib.rs
@@ -7,9 +7,36 @@ pub mod lottery_program {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let program_state = &mut ctx.accounts.program_state;
+        let owner = &mut ctx.accounts.owner;
+
+        program_state.lottery_count = 0;
+        program_state.owner = *owner.key;
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer=owner,
+        space=8+32+64,
+        seeds=[b"state".as_ref()],
+        bump
+    )]
+    pub program_state: Account<'info, ProgramState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+
+#[account]
+pub struct ProgramState {
+    pub owner: Pubkey,
+    pub lottery_count: u64
+}
--- tests/lottery_program.ts
+++ tests/lottery_program.ts
@@ -1,16 +1,34 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import { assert } from "chai";
 import { LotteryProgram } from "../target/types/lottery_program";
 
+var provider = anchor.AnchorProvider.env();
+const owner_key = provider.wallet.publicKey;
+const system_key = anchor.web3.SystemProgram.programId
 describe("lottery_program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.LotteryProgram as Program<LotteryProgram>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+    let [account_state_signer] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("state")],
+      program.programId
+    )
+    const tx = await program.methods.initialize().accounts({
+      programState: account_state_signer,
+      owner: owner_key,
+      systemProgram: system_key
+    })
+    .rpc();
+    console.log("Your initialized transaction signature", tx);
+
+    let created_state = await program.account.programState.fetch(account_state_signer);
+    assert(created_state.owner.toString() === owner_key.toString(), "Owner do not match.")
   });
 });
