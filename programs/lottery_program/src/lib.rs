de6fb62bf1c87a2f20f2470321e16ee83b35ddb1
Jaymanyoo Singh Chouhan <jaymanyoo@Jaymanyoos-Mac-mini.local>
2022-07-10 19:28:18 +0530

    create_participant,draw_winner, claim_reward

--- programs/lottery_program/src/lib.rs
+++ programs/lottery_program/src/lib.rs
@@ -5,7 +5,9 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod lottery_program {
     use super::*;
-
+    use anchor_lang::solana_program::system_instruction::transfer;
+    use anchor_lang::solana_program::program::invoke;
+    
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let program_state = &mut ctx.accounts.program_state;
         let owner = &mut ctx.accounts.owner;
@@ -16,7 +18,7 @@ pub mod lottery_program {
         Ok(())
     }
 
-    pub fn create_lottery(ctx: Context<CreateLottery>) -> Result<()> {
+    pub fn create_lottery(ctx: Context<CreateLottery>, entry_fee:u64) -> Result<()> {
         let program_state = &mut ctx.accounts.program_state;
         let user = &mut ctx.accounts.user;
         let lottery = &mut ctx.accounts.lottery;
@@ -24,13 +26,100 @@ pub mod lottery_program {
         // check if the program_state "owner" is equal to "user"
         require!(*user.key == program_state.owner, LotteryError::NotAManager);
 
+        lottery.index = program_state.lottery_count;
+        lottery.entry_fee = entry_fee;
         lottery.participant_count = 0;
+        lottery.funds = 0;
         lottery.is_active = true;
+        lottery.reward_claimed = false;
 
         program_state.lottery_count += 1;
 
         Ok(())
     }
+
+    pub fn create_participant(ctx: Context<CreateParticipant>) -> Result<()> {
+        let participant = &mut ctx.accounts.participant;
+        let user = &mut ctx.accounts.user;
+        let lottery = &mut ctx.accounts.lottery;
+
+        // check if the user has alredy participated
+        // Logic pending for this check
+
+        let ix = transfer(&user.key, &lottery.key(), lottery.entry_fee);
+
+        invoke(&ix, &[
+            user.to_account_info(),
+            lottery.to_account_info(),
+        ]);
+
+        participant.user = *user.key;
+        participant.index = lottery.participant_count;
+
+        lottery.funds += lottery.entry_fee;
+        lottery.participant_count += 1;
+
+        Ok(())
+    }
+
+    pub fn finalize_winner(ctx: Context<FinalizeWinner>) -> Result<()> {
+        let program_state = &mut ctx.accounts.program_state;
+        let lottery = &mut ctx.accounts.lottery;
+        let user = &mut ctx.accounts.user;
+
+        // Check if the user is the owner of state
+        require!(program_state.owner == *user.key, LotteryError::NotAManager);
+
+        let mut participant_list = Vec::new();
+        for index in 0..lottery.participant_count {
+            let (expected_pda, bump) = Pubkey::find_program_address(&[b"participant", &lottery.index.to_be_bytes(), &index.to_be_bytes()], ctx.program_id);
+            msg!("Index = {}, bump={}, pda = {}", index, bump, expected_pda);
+            participant_list.push(expected_pda)
+        }
+        
+        let choosen_winner_participant:Pubkey = participant_list[0]; // TODO: generate it randomly
+
+        msg!("Winner is {}", choosen_winner_participant);
+        lottery.winner = choosen_winner_participant;
+        lottery.is_active = false;
+
+
+        Ok(())
+    }
+
+    pub fn claim_reward(ctx: Context<ClaimReward>) -> Result<()> {
+        let lottery = &mut ctx.accounts.lottery;
+        let user = &mut ctx.accounts.user;
+        let participant = &mut ctx.accounts.participant;
+
+        msg!("winner = {} and claimer={}", lottery.winner, *user.key);
+
+        require!(participant.key() == lottery.winner, LotteryError::NotAWinner);
+        require!(participant.user == *user.key, LotteryError::NotAWinner);
+        require!(lottery.reward_claimed == false, LotteryError::RewardAlreadyClaimed);
+        
+        transfer_service_fee_lamports(&lottery.to_account_info(), user, lottery.funds)?;
+
+        // Set prize claimed as true
+        lottery.reward_claimed = true;
+
+        Ok(())
+    }
+
+    
+}
+fn transfer_service_fee_lamports(
+    from_account: &AccountInfo,
+    to_account: &AccountInfo,
+    amount_of_lamports: u64,
+) -> Result<()> {
+    // Does the from account have enough lamports to transfer?
+    require!(**from_account.try_borrow_lamports()? > amount_of_lamports, LotteryError::InsufficientFundsForTransaction);
+
+    // Debit from_account and credit to_account
+    **from_account.try_borrow_mut_lamports()? -= amount_of_lamports;
+    **to_account.try_borrow_mut_lamports()? += amount_of_lamports;
+    Ok(())
 }
 
 #[derive(Accounts)]
@@ -47,7 +136,7 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
 
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -62,7 +151,7 @@ pub struct CreateLottery<'info> {
     #[account(
         init,
         payer=user,
-        space=8+32+32+64+2,
+        space=8+64+64+64+32+64+2+2,
         seeds=[b"lottery".as_ref(), program_state.lottery_count.to_be_bytes().as_ref()],
         bump
     )]
@@ -71,26 +160,113 @@ pub struct CreateLottery<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
+
+#[derive(Accounts)]
+pub struct CreateParticipant<'info> {
+    #[account(
+        mut,
+        seeds=[b"lottery".as_ref(), lottery.index.to_be_bytes().as_ref()],
+        bump,
+    )]
+    pub lottery: Account<'info, Lottery>,
+
+    #[account(
+        init,
+        payer=user,
+        space=8+32+64,
+        seeds=[b"participant".as_ref(), lottery.index.to_be_bytes().as_ref(), lottery.participant_count.to_be_bytes().as_ref()],
+        bump
+    )]
+    pub participant: Account<'info, Participant>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+
+#[derive(Accounts)]
+pub struct FinalizeWinner<'info> {
+    #[account(
+        mut,
+        seeds=[b"state".as_ref()],
+        bump
+    )]
+    pub program_state: Account<'info, ProgramState>,
+
+    #[account(
+        mut,
+        seeds=[b"lottery".as_ref(), lottery.index.to_be_bytes().as_ref()],
+        bump,
+    )]
+    pub lottery: Account<'info, Lottery>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ClaimReward<'info> {
+
+    #[account(
+        mut,
+        seeds=[b"lottery".as_ref(), lottery.index.to_be_bytes().as_ref()],
+        bump,
+    )]
+    pub lottery: Account<'info, Lottery>,
+
+    #[account(
+        mut,
+        seeds=[b"participant".as_ref(), lottery.index.to_be_bytes().as_ref(), participant.index.to_be_bytes().as_ref()],
+        bump
+    )]
+    pub participant: Account<'info, Participant>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
 
 #[account]
 pub struct ProgramState {
     pub owner: Pubkey,
-    pub lottery_count: u64
+    pub lottery_count: u64,
 }
 
 #[account]
 pub struct Lottery {
-    pub escrow: Pubkey,
+    pub index: u64,
+    pub funds: u64,
+    pub entry_fee: u64,
     pub winner: Pubkey,
     pub participant_count: u64,
     pub is_active: bool,
+    pub reward_claimed: bool
+}
+
+#[derive(std::fmt::Debug)]
+#[account]
+pub struct Participant {
+    user: Pubkey,
+    index: u64
 }
 
 #[error_code]
 pub enum LotteryError {
     #[msg("Restricted! You are not having Manager privilege.")]
-    NotAManager
+    NotAManager,
+
+    #[msg("Claim Failed! You are not winner of this lottery.")]
+    NotAWinner,
+
+    #[msg("Claim Failed! Lottery reward is already claimed.")]
+    RewardAlreadyClaimed,
+
+    #[msg("Claim Failed! Insufficiant funds.")]
+    InsufficientFundsForTransaction,
 }
--- tests/lottery_program.ts
+++ tests/lottery_program.ts
@@ -7,6 +7,8 @@ import { LotteryProgram } from "../target/types/lottery_program";
 var provider = anchor.AnchorProvider.env();
 const owner_key = provider.wallet.publicKey;
 const system_key = anchor.web3.SystemProgram.programId
+
+const user_jonny = anchor.web3.Keypair.generate();
 describe("lottery_program", () => {
   // Configure the client to use the local cluster.
 
@@ -44,7 +46,7 @@ describe("lottery_program", () => {
       [utf8.encode("lottery"), account_state_data.lotteryCount.toBuffer('be', 8)],
       program.programId
     )
-    const tx = await program.methods.createLottery()
+    const tx = await program.methods.createLottery(new anchor.BN(100000))
       .accounts({
         programState: account_state_pda,
         lottery: lottery_pda,
@@ -53,10 +55,10 @@ describe("lottery_program", () => {
       })
       .rpc();
 
-      let created_lottery = await program.account.lottery.fetch(lottery_pda);
-      account_state_data = await program.account.programState.fetch(account_state_pda);
-      assert(created_lottery.participantCount.toString() === "0", "Wrong Lottery Participant count.")
-      assert(account_state_data.lotteryCount.toString() === "1", "Wrong Lottery count in program.")
+    let created_lottery = await program.account.lottery.fetch(lottery_pda);
+    account_state_data = await program.account.programState.fetch(account_state_pda);
+    assert(created_lottery.participantCount.toString() === "0", "Wrong Lottery Participant count.")
+    assert(account_state_data.lotteryCount.toString() === "1", "Wrong Lottery count in program.")
   });
 
   it("Non-manager fails creating lottery room!", async () => {
@@ -76,23 +78,167 @@ describe("lottery_program", () => {
     let bad_actor = anchor.web3.Keypair.generate();
     await fundAccount(bad_actor);
 
-    try{
-      const tx = await program.methods.createLottery()
+    try {
+      const tx = await program.methods.createLottery(new anchor.BN(100000))
+        .accounts({
+          programState: account_state_pda,
+          lottery: lottery_pda,
+          user: bad_actor.publicKey,
+          systemProgram: system_key
+        })
+        .signers([bad_actor])
+        .rpc();
+
+      assert(false, "unauthorised user created lottery!")
+    } catch (err) {
+      // console.log(err);
+
+    }
+
+  });
+
+  it("Take part in lottery!", async () => {
+    let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("lottery"), new anchor.BN(0).toBuffer('be', 8)],
+      program.programId
+    )
+    let lottery_data = await program.account.lottery.fetch(lottery_pda);
+
+    let [participant_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("participant"), lottery_data.index.toBuffer('be', 8), lottery_data.participantCount.toBuffer('be', 8)],
+      program.programId
+    )
+    console.log("participant pda 1 =", participant_pda.toString())
+    // create a unauthorised user
+
+    await fundAccount(user_jonny);
+    await getBalance("lottery before", lottery_pda);
+    await getBalance("jonny before", user_jonny.publicKey);
+
+    const tx = await program.methods.createParticipant()
+      .accounts({
+        lottery: lottery_pda,
+        participant: participant_pda,
+        user: user_jonny.publicKey
+      })
+      .signers([user_jonny])
+      .rpc()
+
+    console.log("Your participation transaction signature=", tx);
+    await getBalance("lottery after", lottery_pda);
+    await getBalance("jonny after", user_jonny.publicKey);
+  });
+  // it("Take part in lottery Again!", async () => {
+  //   let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+  //     [utf8.encode("lottery"), new anchor.BN(0).toBuffer('be', 8)],
+  //     program.programId
+  //   )
+  //   let lottery_data = await program.account.lottery.fetch(lottery_pda);
+
+  //   let [participant_pda] = await anchor.web3.PublicKey.findProgramAddress(
+  //     [utf8.encode("participant"), lottery_data.index.toBuffer('be', 8), lottery_data.participantCount.toBuffer('be', 8)],
+  //     program.programId
+  //   )
+  //   console.log("participant pda 2 =", participant_pda)
+  //   // create a unauthorised user
+
+  //   await fundAccount(user_jonny);
+
+  //   const tx = await program.methods.createParticipant()
+  //     .accounts({
+  //       lottery: lottery_pda,
+  //       participant: participant_pda,
+  //       user: user_jonny.publicKey
+  //     })
+  //     .signers([user_jonny])
+  //     .rpc()
+
+  //   console.log("Your participation transaction signature=", tx);
+  // });
+
+  // it("Jonny take part in again in same lottery!", async () => {
+  //   let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+  //     [utf8.encode("lottery"), new anchor.BN(0).toBuffer('be', 8)],
+  //     program.programId
+  //   )
+  //   let lottery_data = await program.account.lottery.fetch(lottery_pda);
+
+  //   let [participant_pda] = await anchor.web3.PublicKey.findProgramAddress(
+  //     [utf8.encode("participant"), lottery_data.participantCount.toBuffer('be', 8)],
+  //     program.programId
+  //   )
+
+  //   // create a unauthorised user
+  //   await fundAccount(user_jonny);
+
+  //   const tx = await program.methods.createParticipant()
+  //     .accounts({
+  //       lottery: lottery_pda,
+  //       participant: participant_pda,
+  //       user: user_jonny.publicKey
+  //     })
+  //     .signers([user_jonny])
+  //     .rpc()
+
+  //   console.log("Your participation transaction signature=", tx);
+  // });
+
+  it("Finalize the winner!", async () => {
+    let [account_state_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("state")],
+      program.programId
+    )
+
+    let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("lottery"), new anchor.BN(0).toBuffer('be', 8)],
+      program.programId
+    )
+    await getBalance("lottery before", lottery_pda);
+    await getBalance("jonny before", user_jonny.publicKey);
+    let tx = await program.methods.finalizeWinner()
       .accounts({
         programState: account_state_pda,
         lottery: lottery_pda,
-        user: bad_actor.publicKey,
-        systemProgram: system_key
+        user: owner_key,
       })
-      .signers([bad_actor])
-      .rpc();
+      .rpc()
 
-      assert(false, "unauthorised user created lottery!")
-    }catch(err){
-      // console.log(err);
-      
-    }
     
+  });
+
+  it("List of participant PDA derived!", async () => {
+    for await (let i of [0]) {
+      let [participant_pda, bump] = await anchor.web3.PublicKey.findProgramAddress(
+        [utf8.encode("participant"), new anchor.BN(0).toBuffer('be', 8), new anchor.BN(i).toBuffer('be', 8)],
+        program.programId
+      )
+      console.log("participant pad", { i }, "=", participant_pda.toString(), " bump=", bump)
+    }
+
+  });
+
+  it("Jonny claims his prize!", async () => {
+    let [lottery_pda] = await anchor.web3.PublicKey.findProgramAddress(
+      [utf8.encode("lottery"), new anchor.BN(0).toBuffer('be', 8)],
+      program.programId
+    );
+
+    let lottery_data = await program.account.lottery.fetch(lottery_pda);
+      let lottery_winning_participant = lottery_data.winner;
+    let tx = await program.methods.claimReward()
+    .accounts({
+      lottery: lottery_pda,
+      participant: lottery_winning_participant,
+      user: user_jonny.publicKey,
+      systemProgram: system_key
+    })
+    .signers([user_jonny])
+    .rpc();
+    console.log("Claimer is =", user_jonny.publicKey.toString())
+    lottery_data = await program.account.lottery.fetch(lottery_pda);
+    console.log("Lottery Data", lottery_data)
+    await getBalance("lottery after winner", lottery_pda);
+    await getBalance("jonny after winner", user_jonny.publicKey);
   });
 });
 
@@ -104,3 +250,9 @@ const fundAccount = async (userKey) => {
 
   let balance = await connection.getBalance(userKey.publicKey);
 }
+
+const getBalance = async (text = "", target_pubkey) => {
+  const connection = provider.connection;
+  let balance = await connection.getBalance(target_pubkey);
+  console.log(text, "=>", balance)
+} 
